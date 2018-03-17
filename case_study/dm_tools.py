bfa7c1c594a3e657b67de33901edf72f625e22d2
Camilla Tran <camilla@Camillas-MacBook-Pro.local>
2018-03-18 09:48:08 +1000

    Fikset task1

--- case_study/dm_tools.py
+++ case_study/dm_tools.py
@@ -1,22 +1,30 @@
 import numpy as np
 import pandas as pd
 
+
 def data_prep():
     df = pd.read_csv('organics.csv')
-    df.drop(['CUSTID', 'DOB', 'EDATE', 'NEIGHBORHOOD','LCDATE'], axis=1, inplace=True)
+    
+    # Justify why we dropp all these values
+    df.drop(['CUSTID', 'DOB', 'EDATE', 'NEIGHBORHOOD','LCDATE', 'BILL', 'AGEGRP1', 'AGEGRP2'], axis=1, inplace=True)
     
     #Fill in mean values for nan in numeric values, explored that AGE, AFFL and LTIME has NaN. 
     numeric = ['AGE','AFFL' ,'LTIME']
     
+    #Avoid decimals, use mediam
+    
     for i in numeric:
-        df[i].fillna(df[i].mean(), inplace=True)
+        df[i].fillna(df[i].median(), inplace=True)
     
     #Some values are out of range in AFFL, converting them to mean values
-    afflmean = df['AFFL'].mean()
+    afflmedian = df['AFFL'].median()
     
     for i in range(len(df['AFFL'])):
         if df['AFFL'][i] < 1 or df['AFFL'][i] > 30:
-            # replace all 0, 34, and 31 with mean()
-            df['AFFL'].replace(df['AFFL'][i],afflmean, inplace = True)
+            # replace all 0, 34, and 31 with median()
+            df['AFFL'].replace(df['AFFL'][i],afflmedian, inplace = True)
+    
+    #Fill in GENDER
+    df['GENDER'].fillna('U', inplace = True)
         
     return df
